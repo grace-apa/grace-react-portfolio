ea883adce5718b7375e948c7df2c20d6ab5dc611
grace-apa <149172807+grace-apa@users.noreply.github.com>
2024-02-28 08:57:58 +0000

    files moved

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../index.css";
+import projectsData from "./data/projects.json";
 import musicfy from "../images/Musicfy.png";
 import generatepassword1 from "../images/generatepassword1.png";
 import Weatherapp from "../images/Weatherapp.png";
