8c0320f02230ba234e2740ec60646c304099145b
grace-apa <149172807+grace-apa@users.noreply.github.com>
2024-02-28 07:52:16 +0000

    update to contact component

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -2,47 +2,39 @@ import React from "react";
 
 const ContactForm = () => {
   return (
-    <div className="d-flex justify-content-center">
-      <div className="p-5 mb-4 bg-body-tertiary rounded-3 abstractBackground">
-        <div className="contact-form">
+    <div className="p-5 mb-4 bg-body-tertiary rounded-3 abstractBackground">
+      <div className="container-fluid py-5">
+        <div className="contact-form text-center">
           <h2>Contact Me</h2>
           <br />
           <br />
           <form>
             <div className="form-group">
-              <label htmlFor="name">
-                <strong> Your Name</strong>{" "}
-              </label>
+              <label htmlFor="name">Your Name </label>
               <br />
               <input type="text" id="name" name="name" required />
             </div>
             <br />
             <div className="form-group">
-              <label htmlFor="email">
-                <strong>Your Email</strong>{" "}
-              </label>
+              <label htmlFor="email">Your Email </label>
               <br />
               <input type="email" id="email" name="email" required />
             </div>
             <br />
             <div className="form-group">
-              <label htmlFor="subject">
-                <strong>Subject:</strong>{" "}
-              </label>
+              <label htmlFor="subject">Subject: </label>
               <br />
               <input type="subject" id="subject" name="subject" required />
             </div>
             <br />
             <div className="form-group">
-              <label htmlFor="message">
-                <strong>Message:</strong>{" "}
-              </label>
+              <label htmlFor="message">Message: </label>
               <br />
               <textarea id="message" name="message" rows="4" required />
             </div>
             <br />
             <button className="formButton" type="submit">
-              Submit
+              <strong>Submit</strong>
             </button>
           </form>
           <br />
